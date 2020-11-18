775517349b87640b9db328cc2384363a955a1ee3
Altyelf <ilze.savicka211@gmail.com>
2020-11-18 17:36:42 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+type Color = {
+  count: number;
+  color: string;
+}
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [colors, setColors] = useState<Color[]>([])
+
+  const submitHandler = () => {
+    const newColors = [...colors]
+    if (hasColor(inputValue) === false) {
+      setColors([...colors, { count: 1, color: inputValue }])
+    }
+    else {
+      newColors.map((item) => {
+        if (item.color === inputValue) {
+          return { ...item, count: item.count++ }
+        } else {
+          return item
+        }
+      })
+      setColors(newColors)
+    }
+
+    setInputValue('')
+  }
+
+  const hasColor = (inputColor: string) => {
+    return colors.some((item) => item.color === inputColor)
+  }
+
+  const deleteColor = (color: string) => {
+    const newColors = [...colors]
+    const index = newColors.findIndex(item => item.color === color)
+    newColors.splice(index, 1)
+    setColors(newColors)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type='text'
+        placeholder="add color"
+        value={inputValue}
+        onChange={
+          (e) => {
+            setInputValue(e.target.value)
+          }
+        }
+      />
+
+      <button onClick={submitHandler}>
+        Add
+      </button>
+
+      {colors.map((color, i) => {
+        return (
+          <div
+          onClick={() => deleteColor(color.color)}
+            className='box'
+            key={i}
+            style={{
+              backgroundColor: color.color
+            }}>
+            {color.count}
+          </div>)
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
